93b12fb1ce37feea9f1ac5edfb05a80bd542d55e
Chirag Malhan <chiragmalhan22@gmail.com>
2022-11-28 13:39:26 +0530

    v 1.1

--- Covid-Global-Report/create_case_maps.py
+++ Covid-Global-Report/create_case_maps.py
@@ -4,7 +4,7 @@ import plotly.express as px
 from datetime import datetime, timedelta
 from helper import yesterday
 
-# Local files
+# Local file
 from helper import load_relevant_data
 
 # This function creates a dataframe of the daily global cases and 
@@ -28,15 +28,7 @@ def plot_global_case_map(filename=None, day=None):
                     width=1000,
                     range_color=[0,10000])
 
+
 	fig.update_layout(margin=dict(l=0, r=0, t=70, b=20), title={"font": {"size": 30}, "x":0.5},)
 	filename = filename if filename else "global_chart.png"
-<<<<<<< HEAD
 	fig.write_image(filename, engine='kaleido')
-=======
-	fig.write_image(filename, engine='kaleido')
-
-if __name__ == '__main__':
-	# yesterday = "10/10/20" # Uncomment for testing
-
-	plot_global_case_map(day=yesterday) # saves as global_chart.png by default
->>>>>>> f4d18573d24b8711a0e0354b6b260ba4b1de660a
